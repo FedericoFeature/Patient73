e10103659229fc9ac24ab8d7b729661306468d83
Dimitrios Kitsikidis <31593501+dimitriosxmi@users.noreply.github.com>
2024-02-06 20:10:30 +0100

    Fix camera shape distortion
    + Update with minor changes the scripts/Camera2D script
    
    Notes: Bug the camera is now positioned a bit further away in order to fix all the maze inside as a square, and will from now on always properly adapt to the maze size, without turning into a distorted shape that stretches in unexpected ways.

--- Scripts/Camera2D.gd
+++ Scripts/Camera2D.gd
@@ -8,8 +8,8 @@ func _ready() -> void:
 func adjust_camera():
 	var maze = get_node("/root/main/TileMap") as MazeGen
 	var tile_size = 64
-	var maze_size = Vector2(maze.x_dim + 5, maze.y_dim + 2) * tile_size
-	self.zoom =  Vector2(self.get_viewport().size) / maze_size
+	var maze_size = Vector2(maze.x_dim + 1, maze.y_dim + 1) * tile_size
+	self.zoom =  Vector2(self.get_viewport().size.y, self.get_viewport().size.y) / maze_size
 	var center_cell = Vector2(maze.x_dim, maze.y_dim) / 2
 	center_cell.y -= 0
 	self.global_position = maze.to_global(maze.map_to_local(center_cell))
