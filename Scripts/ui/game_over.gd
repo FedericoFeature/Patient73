2fbb413ce5e300b010313fcc488b9bbc29c983ad
Dimitrios Kitsikidis <31593501+dimitriosxmi@users.noreply.github.com>
2024-02-20 23:10:15 +0100

    Apply requested changes (requested outside of GitHub)

--- Scripts/ui/game_over.gd
+++ Scripts/ui/game_over.gd
@@ -1,8 +1,7 @@
 extends Control
 
-func _on_back_to_menu_button_pressed():
+func _on_go_to_the_menu_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/ui_menus/start_main_menu.tscn")
 
-
-func _on_play_again_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+func _on_try_again_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/ui_menus/gamemode_selection_main_menu.tscn")
